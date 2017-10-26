0294280d2b7f57c71ff5954790d08a4686beafc0
reiyaono <rono@linkbal.co.jp>
2017-10-26 18:27:59 +0900

    Recive用ファイルの追加

---
+++ sqs_recievemessage.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/sqs"
+)
+
+
+func main() {
+    sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+
+    svc := sqs.New(sess)
+    
+    
+    qURL := "https://sqs.ap-northeast-1.amazonaws.com/047827674873/Go_Queue_Test"  
+    //作ったキューのURLをここに代入
+
+    result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+        AttributeNames: []*string{
+            aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+        },
+        MessageAttributeNames: []*string{
+            aws.String(sqs.QueueAttributeNameAll),
+        },
+        QueueUrl:            &qURL,
+        MaxNumberOfMessages: aws.Int64(1),
+        VisibilityTimeout:   aws.Int64(36000),  // 10 hours
+        WaitTimeSeconds:     aws.Int64(0),
+    })
+	
+	//result.Messages
+	fmt.Println(*result)
+	
+    if err != nil {
+        fmt.Println("Error", err)
+        return
+    }
+
+    if len(result.Messages) == 0 {
+        fmt.Println("Received no messages")
+        return
+    }
+ }
